f25565ecd917d214e0510fe5f266a021dbc8c069
al-jabir <aljabir098@gmail.com>
2022-11-02 21:26:37 +0600

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const App = () => {
-  return <div className='text-lg'>App</div>;
+  return (
+    <div className='text-lg text-blue-500 bg-black'>
+      <h1 className='text-center hover:text-orange-500 duration-1000ss p-5 font-bold italic'>
+        I will learn Tailwind css with vite react
+      </h1>
+    </div>
+  );
 };
 
 export default App;
