d67feea4d1b90a2ecaf9a37dfdab5326637f9e6b
al-jabir <aljabir098@gmail.com>
2022-11-03 19:56:42 +0600

    I have all done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
+import Billing from './components/Billing';
+import Business from './components/Business';
+import CardDeal from './components/CardDeal';
+import Clients from './components/Clients';
+import CTA from './components/CTA';
+import FeedBackCard from './components/FeedbackCard';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Stats from './components/Stats';
+import Testimonials from './components/Testimonials';
+import styled from './style';
 
 const App = () => {
   return (
-    <div className='text-lg text-blue-500 bg-black'>
-      <h1 className='text-center hover:text-orange-500 duration-1000ss p-5 font-bold italic'>
-        I will learn Tailwind css with vite react
-      </h1>
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styled.paddingX} ${styled.flexCenter}`}>
+        <div className={`${styled.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styled.flexStart}`}>
+        <div className={`${styled.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styled.paddingX} ${styled.flexStart}`}>
+        <div className={`${styled.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <FeedBackCard />
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 };
